c6e7c5ccce4bd7709d152f1831bf43da5e93cf5e
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-18 19:06:36 -0600

    Common JS

---
+++ CommonJS/index.js
@@ -0,0 +1,10 @@
+//Commos JS require module
+const sum = require ("./sum")
+
+// console.log("Hola mundo");
+
+//objeto global de Node.js
+// console.log("Objeto global de Node",global);
+// console.log("Objeto global de Node",globalThis);
+
+console.log(sum(2, 3));
