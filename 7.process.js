82eb84fa858987754b86893cb56c3de64a540f51
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-19 23:16:34 -0600

    node:process

---
+++ 7.process.js
@@ -0,0 +1,18 @@
+// PROCESS Poporciona informacion y control sobre el proceso actual de ejecucion
+
+//argumentos de entrada
+// console.log(process.argv);
+
+//controlar proceso y su salida
+// process.exit(0);
+
+//podemos controlar procesos del evento
+// process.on("exit",()=>{
+//     //limpiar los recursos
+// })
+
+//current working directory
+console.log(process.cwd());
+
+//plataforma
+console.log(process.platform);
