2e7e3d34a75268a086515000fd9421c107372a76
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-24 01:33:41 -0600

    post pokemon

--- video-2-API-Express/2.routing.js
+++ video-2-API-Express/2.routing.js
@@ -8,19 +8,37 @@ const processRequest = (req, res) => {
     case "GET":
       switch (url) {
         case "/pokemon/ditto":
-            res.setHeader('Content-Type', 'application/json; charset=utf-8')
-            return res.end(JSON.stringify(dittoJSON))
+          res.setHeader("Content-Type", "application/json; charset=utf-8");
+          return res.end(JSON.stringify(dittoJSON));
         default:
-            res.statusCode = 404
-            res.setHeader('Content-Type', 'text/html; charset=utf-8')
-            return res.end('<h1>404</h1>')
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/html; charset=utf-8");
+          return res.end("<h1>404</h1>");
       }
-    // case "POST":
-    //   switch (url) {
-    //     case "/pokemon":
-    //       let body = "";
-    //       res.setHeader("Content-Type", "application/json;charset=utf-8");
-    //   }
+    case "POST":
+      switch (url) {
+        case "/pokemon":
+          let body = "";
+
+          // escuchar el evento data
+          req.on("data", (chunk) => {
+            body += chunk.toString();
+          });
+
+          req.on("end", () => {
+            const data = JSON.parse(body);
+            res.writeHead(201, {
+              "Content-Type": "application/json; charset=utf-8",
+            });
+            data.timestamp = Date.now()
+            res.end(JSON.stringify(data));
+          });
+          break;
+        default:
+          res.statusCode = 404;
+          res.setHeader("Content-Type", "text/plain; charset=utf-8");
+          return res.end("Error 404");
+      }
   }
 };
 
