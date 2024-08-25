c4a327f49d32dcff590208655dc7ce906bf7ecfc
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-24 22:27:39 -0600

    apply middleware

--- video-2-API-Express/3.express.js
+++ video-2-API-Express/3.express.js
@@ -6,7 +6,30 @@ const PORT = process.env.PORT || 1234;
 const app = express();
 app.disable("x-powered-by")
 
+app.use(express.json())
 
+//middleware
+
+// app.use((req, res, next) => {
+//   if (req.method !== 'POST') return next()
+//   if (req.headers['content-type'] !== 'application/json') return next()
+
+//   // solo llegan request que son POST y que tienen el header Content-Type: application/json
+//   let body = ''
+
+//   // escuchar el evento data
+//   req.on('data', chunk => {
+//     body += chunk.toString()
+//   })
+
+//   req.on('end', () => {
+//     const data = JSON.parse(body)
+//     data.timestamp = Date.now()
+//     // mutar la request y meter la información en el req.body
+//     req.body = data
+//     next()
+//   })
+// })
 
 app.get("/pokemon/ditto", (req, res) => {
   res.json(ditto)
