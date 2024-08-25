c06495131b70fd563321d3b2106208e24c9992fa
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-24 22:16:20 -0600

    use express

---
+++ video-2-API-Express/3.express.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const ditto = require("./pokemon/ditto.json");
+
+const PORT = process.env.PORT || 1234;
+
+const app = express();
+app.disable("x-powered-by")
+
+
+
+app.get("/pokemon/ditto", (req, res) => {
+  res.json(ditto)
+});
+
+app.post("/pokemon", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+
+  req.on("end", () => {
+    const data = JSON.parse(body);
+    data.timestamp = Date.now();
+    res.status(201).json(data);
+  });
+});
+
+app.use((req,res)=>{
+    res.status(404).send("<h1>404</h1>")
+})
+
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+});
