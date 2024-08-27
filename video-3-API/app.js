960e32e006aad3c3529d12c1e5185ab6ec8048b5
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-26 23:03:23 -0600

    get genre

--- video-3-API/app.js
+++ video-3-API/app.js
@@ -8,14 +8,22 @@ app.get("/", (req, res) => {
   res.json({ message: "Hola mundo" });
 });
 
-app.get("/movies", (req, res) => {
-  res.json(movies);
-});
+app.get('/movies', (req, res) => {
+    const { genre } = req.query
+    if (genre) {
+      const filteredMovies = movies.filter(
+        movie => movie.genre.some(g => g.toLowerCase() === genre.toLowerCase())
+      )
+      return res.json(filteredMovies)
+    }
+    res.json(movies)
+  })
 
 app.get("/movies/:id", (req, res) => {
   const id = req.params.id;
   const movie = movies.find((movie) => movie.id === id);
-  res.json(movie);
+  if (movie) return res.json(movie);
+  res.status(404).json({ message: "Película no encontrada" });
 });
 
 const PORT = process.env.PORT ?? 1234;
