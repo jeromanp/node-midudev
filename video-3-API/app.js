c7408e11f2ec9c8f3d72f439e5c845a10f5f8fb3
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-08-30 17:40:18 -0600

    use cors

--- video-3-API/app.js
+++ video-3-API/app.js
@@ -1,10 +1,32 @@
 const express = require("express");
 const crypto = require("node:crypto");
+const cors = require(cors);
 const movies = require("./movies.json");
 const { validateMovie, validatePartialMovie } = require("./schemas/movie");
 
 const app = express();
 app.use(express.json());
+app.use(
+  cors({
+    origin: (origin, callback) => {
+      const ACCEPTED_ORIGINS = [
+        "http://localhost:8080",
+        "http://localhost:1234",
+        "https://movies.com",
+      ];
+
+      if (ACCEPTED_ORIGINS.includes(origin)) {
+        return callback(null, true);
+      }
+
+      if (!origin) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS"));
+    },
+  })
+);
 app.disable("x-powered-by"); //deshabilita el header X-Powered-By: Express
 
 app.get("/", (req, res) => {
