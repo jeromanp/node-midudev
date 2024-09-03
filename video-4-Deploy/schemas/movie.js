56cbabd1313e9851225cadf5b50344ebde9463d4
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-02 22:30:07 -0600

    copy api

---
+++ video-4-Deploy/schemas/movie.js
@@ -0,0 +1,46 @@
+const z = require("zod");
+
+const movieSchema = z.object({
+  title: z.string({
+    invalid_type_error: "El título debe ser una cadena de texto",
+    required_error: "El título es obligatorio",
+  }),
+  genre: z.array(
+    z.enum([
+      "Action",
+      "Adventure",
+      "Comedy",
+      "Drama",
+      "Fantasy",
+      "Horror",
+      "Thriller",
+      "Sci-Fi",
+      "Crime",
+    ]),
+    {
+      required_error: "El género es obligatorio",
+      invalid_type_error: "El género debe ser una opción válida",
+    }
+  ),
+  year: z.number().int().min(1900).max(2024),
+  director: z.string(),
+  duration: z.number().int().positive().min(0),
+  rate: z.number().min(0).max(10).default(5),
+  poster: z.string().url({
+    message: "El poster debe ser una URL válida",
+  }),
+});
+
+function validateMovie(object) {
+  return movieSchema.safeParse(object);
+}
+
+//validar patch con movieSchema
+function validatePartialMovie(input) {
+  return movieSchema.partial().safeParse(input);
+}
+
+module.exports = {
+  validateMovie,
+  validatePartialMovie,
+};
