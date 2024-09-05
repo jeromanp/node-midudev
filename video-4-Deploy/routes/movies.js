c67f321a680a3c785892b2750f3ce61328007c58
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-04 21:27:16 -0600

    add controlles

--- video-4-Deploy/routes/movies.js
+++ video-4-Deploy/routes/movies.js
@@ -1,54 +1,10 @@
 import { Router } from "express";
-import { validateMovie, validatePartialMovie } from "../schemas/movie.js";
-import { MovieModel } from "../models/movie.js";
+import { MovieController } from "../controllers/movies.js";
 
 export const moviesRouter = Router();
 
-moviesRouter.get("/", async (req, res) => {
-  const { genre } = req.query;
-  const movies = await MovieModel.getAll({ genre });
-  res.json(movies);
-});
-
-moviesRouter.get("/:id", async (req, res) => {
-  const {id} = req.params;
-  const movie = await MovieModel.getById({ id });
-  if (movie) return res.json(movie);
-  res.status(404).json({ message: "Película no encontrada" });
-});
-
-moviesRouter.post("/", async (req, res) => {
-  const result = validateMovie(req.body);
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const newMovie = await MovieModel.create({ input: result.data });
-  res.status(201).json(newMovie);
-});
-
-moviesRouter.patch("/:id", async (req, res) => {
-  const result = validatePartialMovie(req.body);
-
-  if (!result.success) {
-    return res.status(400).json({ error: JSON.parse(result.error.message) });
-  }
-
-  const { id } = req.params;
-  const updateMovie = await MovieModel.update({ id, input: result.data });
-
-  return res.json(updateMovie);
-});
-
-moviesRouter.delete("/:id", async (req, res) => {
-  const { id } = req.params;
-
-  const result = await MovieModel.delete({ id });
-
-  if (result === false) {
-    return res.status(404).json({ message: "Movie not found" });
-  }
-
-  return res.json({ message: "Movie deleted" });
-});
+moviesRouter.get("/", MovieController.getAll);
+moviesRouter.get("/:id", MovieController.getById);
+moviesRouter.post("/", MovieController.create);
+moviesRouter.patch("/:id", MovieController.update);
+moviesRouter.delete("/:id", MovieController.delete);
