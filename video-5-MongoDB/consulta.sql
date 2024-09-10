d1c3ac71db9a72c4303182ce5d7b4332d7c5dc43
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-10 11:17:14 -0600

    update sql, create, insert tables

--- video-5-MongoDB/consulta.sql
+++ video-5-MongoDB/consulta.sql
@@ -1,11 +1,9 @@
-DROP TABLE IF EXISTS moviesdb;
---crear base de datos
+DROP DATABASE IF EXISTS moviesdb;
 CREATE DATABASE moviesdb;
---usar base de datos
 USE moviesdb;
---crear la tabla movies    
+
 CREATE TABLE movies (
-	id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BINARY(UUID())),
+	id BINARY(16) PRIMARY KEY DEFAULT (UUID_TO_BIN(UUID())),
 	title VARCHAR(255) NOT NULL,
 	year INT NOT NULL,
 	director VARCHAR(255) NOT NULL,
@@ -13,18 +11,17 @@ CREATE TABLE movies (
 	poster TEXT,
 	rate DECIMAL(2,1) UNSIGNED NOT NULL
 );
---crear tabla genero
+
 CREATE TABLE genres (
-	id INT  AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL UNIQUE,	
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL UNIQUE
 );
 
-CREATE TABLE movies_genre (
+CREATE TABLE movies_genres (  -- Cambiado a "movies_genres" para coincidir con la inserción
 	movie_id BINARY(16) REFERENCES movies(id),
-	genre_id INT REFERENCES genres(id)
-	PRIMARY KEY (movie_id,genre_id)
+	genre_id INT REFERENCES genres(id),
+	PRIMARY KEY (movie_id, genre_id)
 );
-
 
 INSERT INTO genres (name) VALUES 
 ("Drama"),
@@ -36,25 +33,25 @@ INSERT INTO genres (name) VALUES
 ("Animation"),
 ("Biography");
 
-INSERT INTO movies (id, title,year, director,duration,poster,rate) VALUES
-(UUID_TO_BINARY(UUID()), "The Shawshank Redemption",1994,"Frank Darabont",142,"https://i.ebayimg.com/images/g/4goAAOSwMyBe7hnQ/s-l1200.webp",9.3),
-(UUID_TO_BINARY(UUID()),"The Dark Knight",2008,"Christopher Nolan",152,"https://i.ebayimg.com/images/g/yokAAOSw8w1YARbm/s-l1200.jpg",9.0),
-(UUID_TO_BINARY(UUID()),"Inception",2010,"Christopher Nolan",148,"https://m.media-amazon.com/images/I/91Rc8cAmnAL._AC_UF1000,1000_QL80_.jpg",8.8),
-(UUID_TO_BINARY(UUID()),"Pulp Fiction",1994,"Quentin Tarantino",154,"https://www.themoviedb.org/t/p/original/vQWk5YBFWF4bZaofAbv0tShwBvQ.jpg",8.9),
-(UUID_TO_BINARY(UUID()),"Forrest Gump",1994,"Robert Zemeckis",142,"https://i.ebayimg.com/images/g/qR8AAOSwkvRZzuMD/s-l1600.jpg",8.8);
-
+INSERT INTO movies (id, title, year, director, duration, poster, rate) VALUES
+(UUID_TO_BIN(UUID()), "The Shawshank Redemption", 1994, "Frank Darabont", 142, "https://i.ebayimg.com/images/g/4goAAOSwMyBe7hnQ/s-l1200.webp", 9.3),
+(UUID_TO_BIN(UUID()), "The Dark Knight", 2008, "Christopher Nolan", 152, "https://i.ebayimg.com/images/g/yokAAOSw8w1YARbm/s-l1200.jpg", 9.0),
+(UUID_TO_BIN(UUID()), "Inception", 2010, "Christopher Nolan", 148, "https://m.media-amazon.com/images/I/91Rc8cAmnAL._AC_UF1000,1000_QL80_.jpg", 8.8),
+(UUID_TO_BIN(UUID()), "Pulp Fiction", 1994, "Quentin Tarantino", 154, "https://www.themoviedb.org/t/p/original/vQWk5YBFWF4bZaofAbv0tShwBvQ.jpg", 8.9),
+(UUID_TO_BIN(UUID()), "Forrest Gump", 1994, "Robert Zemeckis", 142, "https://i.ebayimg.com/images/g/qR8AAOSwkvRZzuMD/s-l1600.jpg", 8.8);
 
 INSERT INTO movies_genres (movie_id, genre_id) VALUES 
-((SELECT id FROM movies WHERE title="The Shawshank Redemption")(SELECT id FROM genres WHERE name="Drama")),
-((SELECT id FROM movies WHERE title="The Dark Knight")(SELECT id FROM genres WHERE name="Action")),
-((SELECT id FROM movies WHERE title="The Dark Knight")(SELECT id FROM genres WHERE name="Crime")),
-((SELECT id FROM movies WHERE title="The Dark Knight")(SELECT id FROM genres WHERE name="Drama")),
-((SELECT id FROM movies WHERE title="Inception")(SELECT id FROM genres WHERE name="Action")),
-((SELECT id FROM movies WHERE title="Inception")(SELECT id FROM genres WHERE name="Adventure")),
-((SELECT id FROM movies WHERE title="Inception")(SELECT id FROM genres WHERE name="Sci-Fi")),
-((SELECT id FROM movies WHERE title="Pulp Fiction")(SELECT id FROM genres WHERE name="Crime")),
-((SELECT id FROM movies WHERE title="Pulp Fiction")(SELECT id FROM genres WHERE name="Drama")),
-((SELECT id FROM movies WHERE title="Forrest Gump")(SELECT id FROM genres WHERE name="Drama"));
-((SELECT id FROM movies WHERE title="Forrest Gump")(SELECT id FROM genres WHERE name="Romance"));
+((SELECT id FROM movies WHERE title="The Shawshank Redemption"), (SELECT id FROM genres WHERE name="Drama")),
+((SELECT id FROM movies WHERE title="The Dark Knight"), (SELECT id FROM genres WHERE name="Action")),
+((SELECT id FROM movies WHERE title="The Dark Knight"), (SELECT id FROM genres WHERE name="Crime")),
+((SELECT id FROM movies WHERE title="The Dark Knight"), (SELECT id FROM genres WHERE name="Drama")),
+((SELECT id FROM movies WHERE title="Inception"), (SELECT id FROM genres WHERE name="Action")),
+((SELECT id FROM movies WHERE title="Inception"), (SELECT id FROM genres WHERE name="Adventure")),
+((SELECT id FROM movies WHERE title="Inception"), (SELECT id FROM genres WHERE name="Sci-Fi")),
+((SELECT id FROM movies WHERE title="Pulp Fiction"), (SELECT id FROM genres WHERE name="Crime")),
+((SELECT id FROM movies WHERE title="Pulp Fiction"), (SELECT id FROM genres WHERE name="Drama")),
+((SELECT id FROM movies WHERE title="Forrest Gump"), (SELECT id FROM genres WHERE name="Drama")),
+((SELECT id FROM movies WHERE title="Forrest Gump"), (SELECT id FROM genres WHERE name="Romance"));
 
-SELECT *,BIN_TO_UUID(id) FROM movies;
+-- Consulta para verificar los datos
+SELECT *, BIN_TO_UUID(id) AS uuid FROM movies;
