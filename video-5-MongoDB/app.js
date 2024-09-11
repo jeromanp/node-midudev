c8b959f0b137e9e1e0611818ca31cf0478dfdf32
Jose Eduardo Roman <jeduardo.roman69@gmail.com>
2024-09-10 22:33:44 -0600

    apply model out app.js

--- video-5-MongoDB/app.js
+++ video-5-MongoDB/app.js
@@ -1,22 +1,22 @@
 import express, { json } from "express";
-// import { moviesRouter } from "./routes/movies.js";
-import { corsMiddleware } from "./middlewares/cors.js";
 import { createMovieRouter } from "./routes/movies.js";
-import { MovieModel } from "./models/mysql/movie.js";
+import { corsMiddleware } from "./middlewares/cors.js";
 
-const app = express();
-app.use(json());
-app.use(corsMiddleware());
-app.disable("x-powered-by"); //deshabilita el header X-Powered-By: Express
+export const createApp = ({ movieModel }) => {
+  const app = express();
+  app.use(json());
+  app.use(corsMiddleware());
+  app.disable("x-powered-by"); //deshabilita el header X-Powered-By: Express
 
-app.get("/", (req, res) => {
-  res.json({ message: "Hola mundo" });
-});
+  app.get("/", (req, res) => {
+    res.json({ message: "Hola mundo" });
+  });
 
-app.use("/movies", createMovieRouter({ movieModel: MovieModel }));
+  app.use("/movies", createMovieRouter({ movieModel }));
 
-const PORT = process.env.PORT ?? 1234;
+  const PORT = process.env.PORT ?? 1234;
 
-app.listen(PORT, () => {
-  console.log(`Server escuchando en el puerto http://localhost:${PORT}`);
-});
+  app.listen(PORT, () => {
+    console.log(`Server escuchando en el puerto http://localhost:${PORT}`);
+  });
+};
